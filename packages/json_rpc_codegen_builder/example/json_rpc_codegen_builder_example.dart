16f2760c8b90cee85fc752b57c9cb6d12be56511
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-09-26 16:33:16 +0000

    WIP

--- packages/json_rpc_codegen_builder/example/json_rpc_codegen_builder_example.dart
+++ packages/json_rpc_codegen_builder/example/json_rpc_codegen_builder_example.dart
@@ -2,11 +2,30 @@ import 'package:json_rpc_codegen/json_rpc_codegen.dart';
 
 part 'json_rpc_codegen_builder_example.g.dart';
 
+class User {
+  final String firstName;
+  final String lastName;
+
+  const User(this.firstName, this.lastName);
+
+  factory User.fromJson(Map<String, dynamic> json) =>
+      throw UnimplementedError(json.toString());
+}
+
 @jsonRpc
 abstract interface class SampleApi {
   void hello(String name, [int times = 1]);
 
-  void notifyGeneric<T1, T2 extends num>(T1 data, List<T2> measures);
+  void notifyGeneric<T1, T2 extends num>({
+    required T1 data,
+    List<T2>? measures,
+  });
+
+  Future<String> echo(String message);
+
+  Future<User> createUser();
+
+  Future<List<List<User>>> userMatric();
 }
 
 void main() {}
--- packages/json_rpc_codegen_builder/lib/src/generators/client_generator.dart
+++ packages/json_rpc_codegen_builder/lib/src/generators/client_generator.dart
@@ -3,6 +3,7 @@ import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:meta/meta.dart';
+import 'package:source_gen/source_gen.dart';
 
 import 'proxy_spec.dart';
 
@@ -48,38 +49,56 @@ final class ClientGenerator extends ProxySpec {
                 ),
             ),
           )
-          ..methods.addAll(_class.methods.map(_buildMethod)),
+          ..methods.addAll(_class.methods.map(_buildMethod))
+          ..methods.add(_buildBatchMethod()),
       );
 
   Method _buildMethod(MethodElement method) {
     if (method.returnType is VoidType) {
       return _buildNotificationMethod(method);
+    } else if (method.returnType.isDartAsyncFuture ||
+        method.returnType.isDartAsyncFutureOr) {
+      return _buildRequestMethod(method);
+    } else {
+      throw InvalidGenerationSourceError(
+        'The return type of RPC methods must be either '
+        'void or Future<T> or FutureOr<T>',
+        element: method,
+        // ignore: missing_whitespace_between_adjacent_strings
+        todo: 'Change the return type to void or to Future'
+            '<${method.returnType.getDisplayString(withNullability: true)}>',
+      );
     }
-
-    return Method(
-      (b) => b
-        ..name = method.name
-        ..returns = TypeReference((b) => b..symbol = 'void'),
-    );
   }
 
   Method _buildNotificationMethod(MethodElement method) => Method(
-        (b) => b
-          ..name = method.name
-          ..types.addAll(method.typeParameters.map(_buildTypeParameter))
+        (b) => _buildMethodBase(method, b)
           ..returns = TypeReference((b) => b..symbol = 'void')
-          ..requiredParameters.addAll(
-            method.parameters
-                .where((e) => e.isRequiredPositional)
-                .map((e) => _buildParameter(e, true)),
-          )
-          ..optionalParameters.addAll(
-            method.parameters
-                .where((e) => !e.isRequiredPositional)
-                .map((e) => _buildParameter(e, false)),
-          )
-          ..annotations.add(const Reference('override')),
+          ..body = _buildNotificationBody(method),
       );
+
+  Method _buildRequestMethod(MethodElement method) => Method(
+        (b) => _buildMethodBase(method, b)
+          ..returns = _buildType(method.returnType)
+          ..modifier = MethodModifier.async
+          ..body = _buildRequestBody(method),
+      );
+
+  MethodBuilder _buildMethodBase(MethodElement method, MethodBuilder builder) =>
+      builder
+        ..name = method.name
+        ..types.addAll(method.typeParameters.map(_buildTypeParameter))
+        ..requiredParameters.addAll(
+          method.parameters
+              .where((e) => e.isRequiredPositional)
+              .map((e) => _buildParameter(e, true)),
+        )
+        ..optionalParameters.addAll(
+          method.parameters
+              .where((e) => !e.isRequiredPositional)
+              .map((e) => _buildParameter(e, false)),
+        )
+        ..annotations.add(const Reference('override'));
 
   Parameter _buildParameter(ParameterElement parameter, bool positional) =>
       Parameter(
@@ -115,4 +134,120 @@ final class ClientGenerator extends ProxySpec {
               ? _buildType(typeParameter.bound!)
               : null,
       );
+
+  Method _buildBatchMethod() => Method(
+        (b) => b
+          ..name = 'withBatch'
+          ..returns = TypeReference((b) => b..symbol = 'void')
+          ..requiredParameters.add(
+            Parameter(
+              (b) => b
+                ..name = 'callback'
+                ..type = TypeReference((b) => b..symbol = 'Function()'),
+            ),
+          )
+          ..body = _clientRef.property('withBatch').call([
+            refer('callback'),
+          ]).code,
+      );
+
+  Code _buildNotificationBody(MethodElement method) => _buildMethodInvocation(
+        _clientRef.property('sendNotification'),
+        method,
+      ).code;
+
+  Code _buildRequestBody(MethodElement method) {
+    final returnType = method.returnType as InterfaceType;
+    final futureType = returnType.typeArguments.single;
+
+    final invocation = _buildMethodInvocation(
+      _clientRef.property('sendRequest'),
+      method,
+    );
+
+    return Block.of([
+      declareFinal('result', type: refer('dynamic'))
+          .assign(invocation.awaited)
+          .statement,
+      _buildReturn(refer('result'), futureType).returned.statement,
+    ]);
+  }
+
+  Expression _buildReturn(
+    Expression value,
+    DartType type,
+  ) {
+    if (type.isDartCoreIterable || type.isDartCoreList) {
+      final interfaceType = type as InterfaceType;
+      final listType = interfaceType.typeArguments.single;
+      final iterable = value.asA(refer('List')).property('map').call([
+        Method(
+          (b) => b
+            ..requiredParameters.add(
+              Parameter(
+                (b) => b
+                  ..name = 'e'
+                  ..type = refer('dynamic'),
+              ),
+            )
+            ..body = _buildReturn(refer('e'), listType).code,
+        ).closure,
+      ]);
+
+      return type.isDartCoreList
+          ? iterable.property('toList').call(const [])
+          : iterable;
+    } else if (type.isDartCorePrimitive) {
+      return value.asA(_buildType(type));
+    } else {
+      return _buildType(type).newInstanceNamed('fromJson', [
+        value.asA(refer('Map<String, dynamic>')),
+      ]);
+    }
+  }
+
+  Expression _buildMethodInvocation(Expression target, MethodElement method) {
+    final hasPositional = method.parameters.any((e) => e.isPositional);
+    final hasNamed = method.parameters.any((e) => e.isNamed);
+    if (hasPositional && hasNamed) {
+      throw InvalidGenerationSourceError(
+        'An RPC method can have only either named or positional parameters, '
+        'not both',
+        element: method,
+        // ignore: missing_whitespace_between_adjacent_strings
+        todo: 'Make all parameters positional or named',
+      );
+    }
+
+    return target.call([
+      literalString(method.name),
+      if (hasPositional)
+        literalList(
+          [
+            for (final p in method.parameters) refer(p.name),
+          ],
+          TypeReference((b) => b..symbol = 'dynamic'),
+        ),
+      if (hasNamed)
+        literalMap(
+          {
+            for (final p in method.parameters)
+              literalString(p.name): refer(p.name),
+          },
+          TypeReference((b) => b..symbol = 'String'),
+          TypeReference((b) => b..symbol = 'dynamic'),
+        ),
+    ]);
+  }
+}
+
+extension DartTypeX on DartType {
+  bool get isDartCorePrimitive =>
+      isDartCoreBool ||
+      isDartCoreDouble ||
+      isDartCoreInt ||
+      isDartCoreMap ||
+      isDartCoreNull ||
+      isDartCoreNum ||
+      isDartCoreString;
 }
