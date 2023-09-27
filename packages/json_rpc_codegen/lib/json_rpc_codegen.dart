ea490948188ae5cab8260cda203e632d953f7ee4
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-09-27 08:14:40 +0200

    ...

--- packages/json_rpc_codegen/lib/json_rpc_codegen.dart
+++ packages/json_rpc_codegen/lib/json_rpc_codegen.dart
@@ -1,3 +1,4 @@
-export 'package:json_rpc_2/json_rpc_2.dart';
+export 'package:json_rpc_2/json_rpc_2.dart' show Client;
+export 'package:stream_channel/stream_channel.dart' show StreamChannel;
 
 export 'src/annotations.dart';
